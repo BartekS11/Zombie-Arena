8fee1bd9303d0b97f5f3268421d060c17c0c89ba
stahu <marekx71@gmail.com>
2022-11-29 19:58:44 +0100

    Starting to organize project structure

--- ZombieArena.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Zombie.h"
-#include <SFML/Graphics.hpp>
-
-int createBackground(sf::VertexArray& rVA, sf::IntRect arena);
-Zombie* createHorde(int numZombies, sf::IntRect arena);
